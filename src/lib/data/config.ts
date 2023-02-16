45d9b821360eb88524190182e24af29c662d15ad
brunosj <contact@landozone.net>
2023-02-16 11:27:18 +0100

    siteImage

--- src/lib/data/config.ts
+++ src/lib/data/config.ts
@@ -2,7 +2,7 @@ export const website = {
 	siteTitle: `landozone`,
 	siteDescription: `imaginative web development`,
 	siteLanguage: `en`,
-	siteImage: `https:/cms.econcrimelab.com/uploads/lz_logo_seo_b6f16dc875.jpg?updated_at=2023-02-16T08:20:55.179Z`,
+	siteImage: `/lz_logo_seo.jpg`,
 	siteAuthor: `@brunosj`,
 	siteUrl: `https://landozone.net/`,
 	twitter: `@brunosj`
