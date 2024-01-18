c2a325fab4c95884a8a7c38d86f07bad42fa208b
brunosj <contact@landozone.net>
2024-01-18 16:02:15 +0100

    open graph twitter

--- src/lib/components/SEO/OpenGraph.svelte
+++ src/lib/components/SEO/OpenGraph.svelte
@@ -15,16 +15,23 @@
 	<meta property="og:type" content={'website'} />
 	<meta property="og:title" content={pageTitle} />
 	<meta property="og:description" content={metadescription} />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={pageTitle} />
+	<meta name="twitter:description" content={metadescription} />
 	{#if image}
 		<meta property="og:image" content={image.url} />
 		<meta property="og:image:width" content="1200" />
 		<meta property="og:image:height" content="630" />
 		<meta property="og:image:alt" content={image.alt} />
+		<meta name="twitter:image" content={image.url} />
+		<meta name="twitter:image:alt" content={image.alt} />
 	{/if}
 	{#if squareImage}
 		<meta property="og:image" content={squareImage.url} />
 		<meta property="og:image:width" content="500" />
 		<meta property="og:image:height" content="500" />
 		<meta property="og:image:alt" content={squareImage.alt} />
+		<meta name="twitter:image" content={squareImage.url} />
+		<meta name="twitter:image:alt" content={squareImage.alt} />
 	{/if}
 </svelte:head>
